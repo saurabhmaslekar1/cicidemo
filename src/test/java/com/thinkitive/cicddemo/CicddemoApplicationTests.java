d5a5ab4c3ed5f7f179c02f9255fc0942d2335f62
PranaliRTanpure <pranali.tanpure@thinkitive.com>
2023-02-21 19:00:45 +0530

    initial commit

---
+++ src/test/java/com/thinkitive/cicddemo/CicddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkitive.cicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
